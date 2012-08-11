da8c116eb84b7e7d5bb16b227e378ab346bb3b72
zhangxigithub <zhangxi_1989@sina.com>
2012-08-11 20:54:10 +0800

    Initial Commit

---
+++ onGithub/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  onGithub
+//
+//  Created by 张玺 on 12-8-11.
+//  Copyright (c) 2012年 张玺. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ onGithub/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  onGithub
+//
+//  Created by 张玺 on 12-8-11.
+//  Copyright (c) 2012年 张玺. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
