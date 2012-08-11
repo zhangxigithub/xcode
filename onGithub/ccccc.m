13a8a06f0cd8c897178e38df8a2c72efb6bb8d7b
zhangxigithub <zhangxi_1989@sina.com>
2012-08-11 21:14:14 +0800

    new file

---
+++ onGithub/ccccc.m
@@ -0,0 +1,31 @@
+//
+//  ccccc.m
+//  onGithub
+//
+//  Created by 张玺 on 12-8-11.
+//  Copyright (c) 2012年 张玺. All rights reserved.
+//
+
+#import "ccccc.h"
+
+@implementation ccccc
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
